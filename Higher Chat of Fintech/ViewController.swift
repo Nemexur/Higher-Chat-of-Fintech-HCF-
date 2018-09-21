5a3106c24f574bd30b44547291b6be7d7acdbdcb
Alex <alex.milogradsky@gmail.com>
2018-09-21 19:07:42 +0300

    Initial Commit

---
+++ Higher Chat of Fintech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Higher Chat of Fintech
+//
+//  Created by Alex Milogradsky on 21.09.2018.
+//  Copyright © 2018 Alex Milogradsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
